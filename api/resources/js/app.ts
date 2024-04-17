e7d50d73e77bc1eebfcc1b35998968320473cdf2
yokogi-mirailabs <yokogi@mirailabs.co.jp>
2024-04-17 23:28:54 +0900

    quasar設定

--- api/resources/js/app.ts
+++ api/resources/js/app.ts
@@ -6,6 +6,22 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+// Quasar
+import { Quasar, Notify, Loading, Dialog } from 'quasar'
+import quasarLang from 'quasar/lang/ja'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
@@ -15,6 +31,14 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Quasar, {
+                plugins: {
+                    Notify,
+                    Loading,
+                    Dialog,
+                },
+                lang: quasarLang,
+            })
             .mount(el);
     },
     progress: {
