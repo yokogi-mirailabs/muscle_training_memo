98875f687f34ec2bb3b9a26e134b89d07ae61393
yokogi-mirailabs <yokogi@mirailabs.co.jp>
2024-04-23 21:54:25 +0900

    バリデーションメッセージ日本語化、スタイル調整 #1

--- api/resources/js/Pages/Auth/Login.vue
+++ api/resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import Checkbox from '@/Components/Checkbox.vue';
 import GuestLayout from '@/Layouts/GuestLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+
+const q = useQuasar();
+
+const isPassword = ref<boolean>(true);
 
 defineProps<{
     canResetPassword?: boolean;
@@ -18,75 +19,96 @@ const form = useForm({
     remember: false,
 });
 
-const submit = () => {
-    form.post(route('login'), {
+const onHandleLogin = () => {
+    form.post(route('login', {
+        onError: () => {
+            q.notify({
+                message: 'ログインに失敗しました',
+                color: 'negative',
+                position: 'top',
+                timeout: 2000,
+            });
+        },
         onFinish: () => {
             form.reset('password');
         },
-    });
-};
+    }))
+}
 </script>
 
 <template>
     <GuestLayout>
-        <Head title="Log in" />
+        <Head title="ログイン" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+        <div class="row justify-center">
+            <div class="col">
+                <div class="text-info text-bold text-center q-pt-xs text-h6 q-mb-xl">
+                    ログイン
+                </div>
+                <q-card flat class="card q-mx-auto">
+                    <q-card-section class="px-20">
+                        <div :class="{ 'q-px-xl': $q.screen.gt.xs }">
+                            <q-form @submit.prevent="onHandleLogin">
+                                <div>
+                                    <div class="q-mb-sm">メールアドレス</div>
+                                    <q-input
+                                        id="input-email"
+                                        v-model="form.email"
+                                        bg-color="white"
+                                        outlined
+                                        dense
+                                        autofocus
+                                        :error="!!form.errors.email"
+                                        :error-message="form.errors.email"
+                                    ></q-input>
+                                </div>
+                                <div class="q-mt-xs">
+                                    <div class="q-mb-sm">パスワード</div>
+                                    <q-input
+                                        id="input-password"
+                                        v-model="form.password"
+                                        bg-color="white"
+                                        outlined
+                                        dense
+                                        :type="isPassword ? 'password' : 'text'"
+                                        :error="!!form.errors.password"
+                                        :error-message="form.errors.password"
+                                    >
+                                        <template v-slot:append>
+                                            <q-icon
+                                                :name="isPassword ? 'visibility_off' : 'visibility'"
+                                                class="cursor-pointer"
+                                                @click="isPassword = !isPassword"
+                                            />
+                                        </template>
+                                    </q-input>
+                                </div>
+                                <div class="row justify-end">
+                                    <Link :href="route('password.request')" class="text-info link-style">パスワードお忘れの方</Link>
+                                </div>
+                                <div class="q-py-md">
+                                    <q-btn
+                                        class="full-width text-bold q-pa-sm btn-radius"
+                                        label="ログイン"
+                                        color="info"
+                                        no-caps
+                                        unelevated
+                                        max-width="100%"
+                                        type="submit"
+                                        :disabled="form.processing"
+                                    ></q-btn>
+                                </div>
+                            </q-form>
+                        </div>
+                        <q-separator class="q-my-md guest-separator-color"></q-separator>
+                        <div class="row justify-center">
+                            <div class="q-py-xs">
+                                <Link :href="route('register')" class="text-info link-style">新規申し込みはこちら</Link>
+                            </div>
+                        </div>
+                    </q-card-section>
+                </q-card>
+            </div>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
     </GuestLayout>
 </template>
--- api/resources/js/types/index.d.ts
+++ api/resources/js/types/index.d.ts
@@ -3,6 +3,8 @@ export interface User {
     name: string;
     email: string;
     email_verified_at: string;
+    created_at: string;
+    updated_at: string;
 }
 
 export type PageProps<T extends Record<string, unknown> = Record<string, unknown>> = T & {
